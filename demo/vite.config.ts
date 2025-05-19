adf94eeffd789e5c585af358503ae8ecdb505239
Iman Pouya <iman@nebulr.group>
2025-05-19 09:34:02 +0200

    fully functional basic project structure

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	server: {
+		host: '0.0.0.0',        // <== required for container access
+		port: 3001,             // <== optional: fixed port
+		strictPort: true,       // <== optional: fail if port is in use
+		watch: {
+		  usePolling: true      // <== optional: fixes HMR issues in some containers
+		}
+	  }
 });

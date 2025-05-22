1e5248f6dcacf0040cdb4fe470f7be21d752c8cd
Iman Pouya <iman@nebulr.group>
2025-05-22 11:06:31 +0200

    even more restructuring to make builds work

--- nblocks-svelte/svelte.config.js
+++ nblocks-svelte/svelte.config.js
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +5,6 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
 };
 
 export default config;

d7a977395d34f8c24c2320b0f359e1cea5c564ce
Iman Pouya <iman@nebulr.group>
2025-10-02 14:07:27 +0200

    package setting does not exist

--- bridge-svelte/svelte.config.js
+++ bridge-svelte/svelte.config.js
@@ -4,11 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	package: {
-		// Ensure svelte-package outputs to dist used by npm publish
-		dir: 'dist'
-	}
+	preprocess: vitePreprocess()
 };
 
 export default config;
